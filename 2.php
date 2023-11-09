c478ffd0424c947b1ba2b1160a614e14cedb2488
TAREKEGN TADELE <118010414+0947756395@users.noreply.github.com>
2023-11-09 20:32:50 +0300

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<?php
+
+$num = readline("Enter the number: ");
+echo "Multiplication table of ",$num," is\n\n";     
+for ($i = 1; $i <= 10; $i++) {
+  echo "\t", $num, " x ", $i, " = ", $num * $i, "\n";
+}
+
+?>
+</body>
+</html>
